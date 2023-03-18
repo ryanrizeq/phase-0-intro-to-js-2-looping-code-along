ac2f466794a0608418716dd3502ff53d8ab0bf9f
Ryan Rizeq <ryanrizeq@gmail.com>
2023-03-17 20:49:09 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(strings, event) {
+    for (let i = 0; i < strings.length; i++) {
+        messages[i] = `Thank you, ${strings[i]}, for the wonderful ${event} gift!`;
+    }
+
+    return messages;
+}
+
+function countDown(integer) {
+    for (let i = integer; i >= 0; i--) {
+        console.log(integer);
+        integer--;
+    }
+}
